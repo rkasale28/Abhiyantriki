7958e12455ab723a120efc042daac9cd740dbb26
dhairyap99 <dhairya.parekh@somaiya.edu>
2019-01-11 13:41:51 +0530

    Exit toast message has been added.

--- app/src/main/java/org/kjsce/abhiyantriki/abhiyantriki/HomeActivity.java
+++ app/src/main/java/org/kjsce/abhiyantriki/abhiyantriki/HomeActivity.java
@@ -1,6 +1,8 @@
 package org.kjsce.abhiyantriki.abhiyantriki;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
 
 public class HomeActivity extends NavActivity {
 
@@ -10,5 +12,24 @@ public class HomeActivity extends NavActivity {
         setContentView(R.layout.activity_home);
     }
 
+    boolean doubleBackToExitPressedOnce = false;
 
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce = false;
+            }
+        }, 2000);
+    }
 }
