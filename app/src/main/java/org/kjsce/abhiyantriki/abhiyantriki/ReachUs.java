eb348221f145781a318e677b718fd5d9a7655d6c
rkasale28 <rohitkasale28@gmail.com>
2019-02-08 14:28:31 +0530

    Added Follow us in Reach Us Activity

--- app/src/main/java/org/kjsce/abhiyantriki/abhiyantriki/ReachUs.java
+++ app/src/main/java/org/kjsce/abhiyantriki/abhiyantriki/ReachUs.java
@@ -1,15 +1,19 @@
 package org.kjsce.abhiyantriki.abhiyantriki;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 public class ReachUs extends NavActivity {
-
+Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +22,78 @@ public class ReachUs extends NavActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        FloatingActionButton floatingActionButton=(FloatingActionButton) findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent=new Intent(ReachUs.this,HomeActivity.class);
+                startActivity(intent);
+            }        });
+
+        ImageView imageView=(ImageView) findViewById(R.id.facebook);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://www.facebook.com/kjsceabhiyantriki/?ref=br_rs";
+                intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+
+        imageView=(ImageView) findViewById(R.id.twitter);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://twitter.com/kjsabhiyantriki?lang=en";
+                intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+
+        imageView=(ImageView) findViewById(R.id.instagram);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://www.instagram.com/kjscelive/";
+                intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+
+        imageView=(ImageView) findViewById(R.id.snapchat);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://www.snapchat.com/add/kjscelive";
+                intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+
+        imageView=(ImageView) findViewById(R.id.youtube);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://www.youtube.com/channel/UC7Z32X4uNZ8c31NteK3065g";
+                intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+
+        imageView=(ImageView) findViewById(R.id.wordpress);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://kjscelive.wordpress.com/";
+                intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
     }
 }
