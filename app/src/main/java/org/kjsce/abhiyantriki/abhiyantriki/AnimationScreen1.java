90192da8a95d6f6c45a2e2a3e34bf245996948d4
premmody312 <premmody312@gmail.com>
2019-02-11 23:34:44 +0530

    Added Startup Animation Activities with the required resources

---
+++ app/src/main/java/org/kjsce/abhiyantriki/abhiyantriki/AnimationScreen1.java
@@ -0,0 +1,35 @@
+package org.kjsce.abhiyantriki.abhiyantriki;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class AnimationScreen1 extends AppCompatActivity {
+
+
+    private static int TIME_OUT = 850; //Time to launch the another activity
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_animation_screen1);
+        final View myLayout = findViewById(R.id.startabhi);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(AnimationScreen1.this, AnimationScreen2.class);
+                startActivity(i);
+                finish();
+            }
+        }, TIME_OUT);
+    }
+
+}
---
+++ app/src/main/java/org/kjsce/abhiyantriki/abhiyantriki/AnimationScreen2.java
@@ -0,0 +1,13 @@
+package org.kjsce.abhiyantriki.abhiyantriki;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AnimationScreen2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animation_screen2);
+    }
+}
